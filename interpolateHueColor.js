9b2adc8916ffc8d7ec6f099e615ecb2e244b42dc
Jonas Winzen <jonas.winzen@dev-null.info>
2017-03-02 09:27:39 +0100

    improving color conversion algorithm

--- interpolateHueColor.js
+++ interpolateHueColor.js
@@ -1,22 +1,47 @@
 var d3 = require('d3');
 
-
-function hsl2hsv(hue,sat,light){
-  sat*=light<.5?light:1-light;
-  return {
-    h: hue,
-    s: 2*sat/(light+sat),
-    v: light+sat
-  };
+function rgb2hsv(r, g, b) {
+  var computedH;
+  var computedS;
+  var computedV = 0;
+  if (r < 0 || g < 0 || b < 0 || r > 255 || g > 255 || b > 255) {
+    return [0, 0, 0];
+  }
+  r = r / 255;
+  g = g / 255;
+  b = b / 255;
+  var minRGB = Math.min(r, Math.min(g, b));
+  var maxRGB = Math.max(r, Math.max(g, b));
+  if (minRGB == maxRGB) {
+    computedV = minRGB;
+    return [0, 0, computedV];
+  }
+  var d = (r == minRGB) ? g - b : ((b == minRGB) ? r - g : b - r);
+  var h = (r == minRGB) ? 3 : ((b == minRGB) ? 1 : 5);
+  computedH = 60 * (h - d / (maxRGB - minRGB));
+  computedS = (maxRGB - minRGB) / maxRGB;
+  computedV = maxRGB;
+  return [computedH, computedS, computedV];
 }
 
 function colorToHsv(color) {
-  color = d3.hsl(color);
+  color = d3.rgb(color);
+  var hsv = rgb2hsv(color.r, color.g, color.b);
+  console.log(hsv);
   var hue, sat, bri;
-  var hsvCol = hsl2hsv(color.h, color.s, color.l);
-  hue = Math.floor((65536/360) * hsvCol.h);
-  sat = Math.floor(254 * hsvCol.s);
-  bri = Math.ceil(254 * hsvCol.v);
+  hue = Math.floor((65536 / 360) * hsv[0]);
+  sat = Math.floor(255 * hsv[1]);
+  bri = Math.floor(255 * hsv[2]);
+  if (sat > 254) {
+    sat = 254;
+  } else if (sat < 1) {
+    sat = 1;
+  }
+  if (bri > 254) {
+    bri = 254;
+  } else if (bri < 1) {
+    bri = 1;
+  }
   return {
     hue: hue,
     sat: sat,
